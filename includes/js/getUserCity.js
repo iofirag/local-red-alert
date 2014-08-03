083e5f3ea5359dba04eab296c6c52312e338e9eb
ofir agahi <iofirag@gmail.com>
2014-08-04 02:25:31 +0300

    t

--- includes/js/getUserCity.js
+++ includes/js/getUserCity.js
@@ -22,51 +22,54 @@ function getLocation() {
 
 
 function showPositionCity(position) {
-	userLatitude = position.coords.latitude;
-	userLongitude = position.coords.longitude;
+	if (position!= null){
+		userLatitude = position.coords.latitude;
+		userLongitude = position.coords.longitude;
+		
+		//console.log("Latitude: " + position.coords.latitude + "  Longitude: " + position.coords.longitude);
 	
-	//console.log("Latitude: " + position.coords.latitude + "  Longitude: " + position.coords.longitude);
-
-	//apiKey = "AIzaSyDIqDiNyzt3qDt84d_5Lds7cHTPx7xLqaY";
-
-	var method = 'GET';
-	var mapsUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true';
-	var async = true;
-	$.ajax({
-		url : mapsUrl,
-		dataType : 'json',
-		type : 'GET',
-		success : function(res) {
-			//console.log(res.results[0].formatted_address);
-			
-			/* Way A */
-			if (res.results.length>0){
-				for (var i=0; i<res.results[0].address_components.length; i++){
-					if (res.results[0].address_components[i].types[0] == "locality"){
-						userCity = res.results[0].address_components[i].long_name;	
-						break;	
+		//apiKey = "AIzaSyDIqDiNyzt3qDt84d_5Lds7cHTPx7xLqaY";
+	
+		var method = 'GET';
+		var mapsUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true';
+		var async = true;
+		$.ajax({
+			url : mapsUrl,
+			dataType : 'json',
+			type : 'GET',
+			success : function(res) {
+				//console.log(res.results[0].formatted_address);
+				
+				/* Way A */
+				if (res.results.length>0){
+					for (var i=0; i<res.results[0].address_components.length; i++){
+						if (res.results[0].address_components[i].types[0] == "locality"){
+							userCity = res.results[0].address_components[i].long_name;	
+							break;	
+						}
+					}
+					
+					if (userCity==null){
+						/* Way B */
+						address = res.results.formatted_address;	// get key "formatted_address"
+						address = address.split(",");	//split all address string by "," delimiter
+						cityWithSpace = address[1];		//get array[1]
+						userCity = cityWithSpace.substring(1);	//remove the space
 					}
 				}
-				
-				if (userCity==null){
-					/* Way B */
-					address = res.results.formatted_address;	// get key "formatted_address"
-					address = address.split(",");	//split all address string by "," delimiter
-					cityWithSpace = address[1];		//get array[1]
-					userCity = cityWithSpace.substring(1);	//remove the space
-				}
+				//console.log(userCity);
+				//$("#user_data").html(userCity);
+				//alert("Your location is: "+userCity);
+				$(".right_part").eq(1).html(userLatitude+"");		// or -  $(".right_part:eq(1)").html(userCity);		
+			},
+			error : function(res, error) {
+				console.log(arguments);
+				alert(" Can't do because: " + error);
 			}
-			//console.log(userCity);
-			//$("#user_data").html(userCity);
-			//alert("Your location is: "+userCity);
-			$(".right_part").eq(1).html(userLatitude+" "+userLongitude);		// or -  $(".right_part:eq(1)").html(userCity);		
-		},
-		error : function(res, error) {
-			console.log(arguments);
-			alert(" Can't do because: " + error);
-		}
-	});
-
+		});
+	}else {
+		$(".right_part").eq(1).html("error");
+	}
 	/* Show user on map */
 	//var lat = position.coords.latitude;
 	//var lng = position.coords.longitude;
